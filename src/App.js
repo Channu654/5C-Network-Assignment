338afff1b9589d875a1cd1c0756ccdcaca24bd51
channaveer <channaveerpatil999@gmail.com>
2022-09-23 13:26:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Rounting from './Pages/Rounting';
+
+function App() {
+  return (
+    <div className='App'>
+      <Rounting />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Redux/Action.js
@@ -0,0 +1,14 @@
+import * as types from './ActionType';
+import axios from 'axios';
+
+const getRepoData = (payload) => (dispatch) => {
+  dispatch({ type: types.GIT_REPO_REQUEST });
+  return axios
+    .get(`https://api.github.com/users/${payload}/repos`)
+    .then((res) =>
+      dispatch({ type: types.GIT_REPO_SUCCESS, payload: res.data })
+    )
+    .catch((err) => dispatch({ type: types.GIT_REPO_FAILURE, payload: err }));
+};
+
+export { getRepoData };
